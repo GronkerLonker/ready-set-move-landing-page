e9d79d15fa85aafd77eb7cab8c90ddec503bf023
Robert Pazurek <robert.pazurek@tui.com>
2018-08-27 17:49:53 +0200

    Add more translations.

--- i18n/en.js
+++ i18n/en.js
@@ -5,6 +5,121 @@ module.exports = {
 			trainerProfile : 'trainer',
 			pricing: 'pricing',
 			contact: 'contact'
+		},
+		philosophy: {
+			title: 'Your customised, healthy way to enhanced quality of life, functional strength and improved motor control.'
+		},
+		profile: {
+			title: {
+				headline: 'Efficient, effective training has to be one thing above all else: customized.',
+				summary: 'I tailor well proven methods of training and latest knowledge of sports medicine specifically to your needs.<br>Thus you will make measurable and noticeable progress while strengthening your health along the way.'
+			},
+			cv: {
+				headline: 'sports cv',
+				certificates: {
+					fitnesstrainerA: {
+						title: 'Fitness Trainer A',
+						date: 'December 2017'
+					},
+					personaltrainer: {
+						title: 'Personal Trainer',
+						date: 'December 2017'
+					},
+					medicalfitnesstrainer: {
+						title: 'Medical Fitness Trainer',
+						date: 'November 2017'
+					},
+					fitnesstrainerB: {
+						title: 'Fitness Trainer B',
+						date: 'Octobre 2017'
+					}
+				},
+				sports: {
+					movement: {
+						title: 'Movement Training',
+						date: 'since 2016'
+					},
+					bodyweight: {
+						title: 'Body Weight Strength Training',
+						date: 'since 2016'
+					},
+					bouldering: {
+						title: 'Bouldering + Route Setting',
+						date: 'since 2012'
+					},
+					systema: {
+						title: 'RMA Systema',
+						date: '3 years'
+					},
+					escrima: {
+						title: 'Eskrima – stick fighting',
+						date: '1 year'
+					},
+					wingtsun: {
+						title: 'Wing Tsun Kung Fu',
+						date: '2 years'
+					},
+					karate: {
+						title: 'Shotokan Karate',
+						date: '5 years'
+					},
+					judo: {
+						title: 'Competition Judo',
+						date: '5 years'
+					},
+					gymnastics: {
+						title: 'Gymnastics',
+						date: '2 years'
+					}
+				}
+			}
+		},
+		pricing: {
+			title: 'Pricing',
+			conditions: 'A training session lasts 60 minutes. Shorter or longer sessions can be arranged individually.',
+			footer: 'All prices apply to sessions inside of Berlin including commute. Longer trips need special agreements.',
+			perUnit: 'per session',
+			items: {
+				showDetailsButton: 'details',
+				testTraining: {
+					title: 'Test Training',
+					subTitle: 'Try it out',
+					details: '<p><small class="text-muted">Vertrauen und Sympathie sind Schlüsselelemente für ein erfolgreiches Personal Training. Ein Testtraining nach einer kurzen Absprache der Ziele bietet die Ideale Möglichkeit um sich kennen zu lernen und zu schauen ob die Chemie stimmt.</small></p>' +
+					'<p><small class="text-muted">Vielleicht weißt du aber auch noch gar nicht ob Personal Training überhaupt etwas für dich ist und willst dich durch ein allgemeines Testtraining davon überzeugen was es dir bieten kann.</small></p>'
+				},
+				singleSession: {
+					title: 'Single Session',
+					subTitle: 'full felxibility / personalized workout',
+					details: '<p><small class="text-muted">Die Einzelstunde bietet dir die Möglichkeit völlig ungebunden erstmal ein paar Trainingseinheiten durchzuführen.</small></p>' +
+					'<p><small class="text-muted">Auch die Erstellung eines persönlichen Trainingsplans für ein später selbst durchgeführtes Training oder die Überprüfung der Trainingserfolge und eine entsprechende Anpassung des aktuellen Trainingsplans können im Rahmen einer Einzelstunde erfolgen.</small></p>' +
+					'<p><small class="text-muted">Bevor wir das erste Mal zusammen trainieren (egal ob Einzelstunde oder Paket), erfolgt ein persönliches Anamnesegespräch. Dabei ermitteln wir deine Trainingsziele und Umstände um dein Training perfekt auf dich zuschneiden zu können.</small>' +
+						'<br><small>Dieses Gespräch ist in der Einzeleinheit und den Trainingspaketen enthalten.</small></p>'
+				},
+				upgrade: {
+					title: 'Extra Time',
+					subTitle: 'extend your booked session by 30 minutes',
+					details: '<p><small class="text-muted">Richtig intensives Krafttraining? Noch einen Mobilitypart dazu? Wenn du mehr Themenfelder abdecken oder intensiver trainieren möchtest, kannst du eine bereits gebuchte Einheite für einen günstigen Stundenpreis verlängern.</small></p>' +
+					'<p><small class="text-muted">Die Verlängerung muss bei der Terminabsprache angegeben werden, damit ich die Einheit richtig einplanen kann und kann auch dauerhaft für eines der Pakete gebucht werden.</small></p>'
+				},
+				consultation: {
+					title: 'Online Workout Plan',
+					subTitle: '…for body weight strength training',
+					details: '<p><small class="text-muted">Du hast kein Problem damit dich zum Training zu motivieren und hast auch schon eine Idee was du erreichen möchtest aber dir fehlt die richtige Idee wie du effektiv Kraft und Muskelaufbau trainieren kannst? Dann brauchst du vielleicht nur den richtigen Trainingsplan.</small></p>' +
+					'<p><small class="text-muted">Nach einem kurzen online Ausstausch per Mail baue ich dir einen Trainingsplan zusammen, der mit einfachen Mitteln auch zu Hause umgesetzt werden kann. Von Vorteil für deine Performance (aber nicht zwingend notwendig) sind eine Klimmzugstange oder Ringe.</small></p>'
+				},
+				fivePack: {
+					title: '5 sessions',
+					subTitle: 'including preparation dialogue'
+				},
+				tenPack: {
+					title: '10 sessions',
+					subTitle: 'including preparation dialogue'
+				},
+				twentyPack: {
+					title: '20 sessions',
+					subTitle: 'including preparation dialogue'
+				}
+			}
 		}
 	}
 }
