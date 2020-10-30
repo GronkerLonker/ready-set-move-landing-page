973d354bc4e87702d8085c000ca42d9de58e50d2
Robert Pazurek <robert.pazurek@gmail.com>
2020-10-30 09:14:44 +0100

    update for studio

--- i18n/de.js
+++ i18n/de.js
@@ -1,144 +1,158 @@
 module.exports = {
-	entries: {
-		language: {
-			default: true,
-			current: {
-				code: 'de',
-				name: 'Deutsch'
-			},
-			alternate : {
-				code: 'en',
-				name: 'English',
-				link: '/en'
-			}
-		},
-		navItem: {
-			trainerProfile : 'Trainerprofil',
-			pricing: 'Preise',
-			contact: 'Kontakt'
-		},
-		philosophy: {
-			title: 'Dein auf dich zugeschnittener, gesunder Weg zu funktionaler Kraft, verbessertem Körpergefühl und höherem Bewegungsverständnis.'
-		},
-		profile: {
-			title: {
-				headline: 'Effizientes, effektives Training ist vor allem eines: individuell zugeschnitten.',
-				summary: 'Ich passe bewährte Trainingsmethoden und neueste Erkenntnisse der Sportmedizin spezifisch auf dich und deine Bedürfnisse an.<br>Dadurch kannst du mess- und spürbare Fortschritte erzielen und gleichzeitig etwas für deine Gesundheit tun.<br>'
-			},
-			cv: {
-				headline: 'sportlicher Werdegang',
-				certificates: {
-					fitnesstrainerA: {
-						title: 'Fitnesstrainer A',
-						date: 'Dezember 2017'
-					},
-					personaltrainer: {
-						title: 'Personal Trainer',
-						date: 'Dezember 2017'
-					},
-					medicalfitnesstrainer: {
-						title: 'Medizinischer Fitnesstrainer',
-						date: 'November 2017'
-					},
-					fitnesstrainerB: {
-						title: 'Fitnesstrainer B',
-						date: 'Oktober 2017'
-					}
-				},
-				sports: {
-					movement: {
-						title: 'gezieltes Movementtraining',
-						date: 'seit 2016'
-					},
-					bodyweight: {
-						title: 'Body Weight Krafttraining',
-						date: 'seit 2016'
-					},
-					bouldering: {
-						title: 'Bouldern + Routenbau',
-						date: 'seit 2012'
-					},
-					systema: {
-						title: 'RMA Systema',
-						date: '3 Jahre'
-					},
-					escrima: {
-						title: 'Escrima – Stockkampf',
-						date: '1 Jahr'
-					},
-					wingtsun: {
-						title: 'Wing Tsun Kung Fu',
-						date: '2 Jahre'
-					},
-					karate: {
-						title: 'Shotokan Karate',
-						date: '5 Jahre'
-					},
-					judo: {
-						title: 'Wettkampf Judo',
-						date: '5 Jahre'
-					},
-					gymnastics: {
-						title: 'Geräteturnen',
-						date: '2 Jahre'
-					}
-				}
-			}
-		},
-		pricing: {
-			title: 'Preise',
-			conditions: 'Eine Trainingseinheit dauert 60 Minuten. Kürzere oder längere Trainingseinheiten können individuell abgesprochen werden.',
-			footer: 'Alle Preise gelten innerhalb von Berlin inkl. Anreise. Bei längerer Anreise werden die Preise gesondert vereinbart.',
-			perUnit: '/ Einheit',
-			items: {
-				showDetailsButton: 'Details',
-				testTraining: {
-					title: 'Testtraining',
-					subTitle: 'Kennenlernen und ausprobieren',
-					details: '<p><small class="text-muted">Vertrauen und Sympathie sind Schlüsselelemente für ein erfolgreiches Personal Training. Ein Testtraining nach einer kurzen Absprache der Ziele bietet die Ideale Möglichkeit um sich kennen zu lernen und zu schauen ob die Chemie stimmt.</small></p>' +
-					'<p><small class="text-muted">Vielleicht weißt du aber auch noch gar nicht ob Personal Training überhaupt etwas für dich ist und willst dich durch ein allgemeines Testtraining davon überzeugen was es dir bieten kann.</small></p>'
-				},
-				singleSession: {
-					title: 'Einzeleinheit',
-					subTitle: 'Volle Flexibilität / persönlicher Trainingsplan',
-					details: '<p><small class="text-muted">Die Einzelstunde bietet dir die Möglichkeit völlig ungebunden erstmal ein paar Trainingseinheiten durchzuführen.</small></p>' +
-					'<p><small class="text-muted">Auch die Erstellung eines persönlichen Trainingsplans für ein später selbst durchgeführtes Training oder die Überprüfung der Trainingserfolge und eine entsprechende Anpassung des aktuellen Trainingsplans können im Rahmen einer Einzelstunde erfolgen.</small></p>' +
-					'<p><small class="text-muted">Bevor wir das erste Mal zusammen trainieren (egal ob Einzelstunde oder Paket), erfolgt ein persönliches Anamnesegespräch. Dabei ermitteln wir deine Trainingsziele und Umstände um dein Training perfekt auf dich zuschneiden zu können.</small>' +
-						'<br><small>Dieses Gespräch ist in der Einzeleinheit und den Trainingspaketen enthalten.</small></p>'
-				},
-				upgrade: {
-					title: 'Zusatzzeit',
-					subTitle: 'je 30 Minuten Verlängerung für eine gebuchte Einheit',
-					details: '<p><small class="text-muted">Richtig intensives Krafttraining? Noch einen Mobilitypart dazu? Wenn du mehr Themenfelder abdecken oder intensiver trainieren möchtest, kannst du eine bereits gebuchte Einheite für einen günstigen Stundenpreis verlängern.</small></p>' +
-					'<p><small class="text-muted">Die Verlängerung muss bei der Terminabsprache angegeben werden, damit ich die Einheit richtig einplanen kann und kann auch dauerhaft für eines der Pakete gebucht werden.</small></p>'
-				},
-				consultation: {
-					title: 'Trainingsplan (online)',
-					subTitle: 'Body Weight Krafttraining',
-					details: '<p><small class="text-muted">Du hast kein Problem damit dich zum Training zu motivieren und hast auch schon eine Idee was du erreichen möchtest aber dir fehlt die richtige Idee wie du effektiv Kraft und Muskelaufbau trainieren kannst? Dann brauchst du vielleicht nur den richtigen Trainingsplan.</small></p>' +
-					'<p><small class="text-muted">Nach einem kurzen online Ausstausch per Mail baue ich dir einen Trainingsplan zusammen, der mit einfachen Mitteln auch zu Hause umgesetzt werden kann. Von Vorteil für deine Performance (aber nicht zwingend notwendig) sind eine Klimmzugstange oder Ringe.</small></p>'
-				},
-				fivePack: {
-					title: '5er Paket',
-					subTitle: 'inkl. Anamnesegespräch'
-				},
-				tenPack: {
-					title: '10er Paket',
-					subTitle: 'inkl. Anamnesegespräch'
-				},
-				twentyPack: {
-					title: '20er Paket',
-					subTitle: 'inkl. Anamnesegespräch'
-				}
-			}
-		},
-		contact: {
-			title: 'Kontakt',
-			subTitle: 'Überzeugt, dass ich dir bei deinem Training helfen kann?<br>' +
-			'Dann schreib mir doch einfach eine Mail oder ruf mich kurz an und wir vereinbaren einen Termin.',
-			instagram: {
-				followMe: 'Folge mir auf Instagram'
-			}
-		}
-	}
+  entries: {
+    language: {
+      default: true,
+      current: {
+        code: "de",
+        name: "Deutsch",
+      },
+      alternate: {
+        code: "en",
+        name: "English",
+        link: "/en",
+      },
+    },
+    navItem: {
+      trainerProfile: "Trainerprofil",
+      pricing: "Preise",
+      contact: "Kontakt",
+    },
+    philosophy: {
+      title:
+        "Dein auf dich zugeschnittener, gesunder Weg zu funktionaler Kraft, verbessertem Körpergefühl und höherem Bewegungsverständnis.",
+    },
+    studio: {
+      heading: "Trainining sofort in meinem neuen Studio!",
+      description:
+        "Ich habe ein eigenes Trainingsstudio eröffnet um noch besser mit Dir trainieren zu können. Neben allen nötigen Geräten für ein ausgeglichenes Krafttraining, gibt es dort ab Dezember 2020 auch eine Kletterwand und einen Hangelparkour. Dass bedeutet in Zukunft Bewegungstraining komplett in 3D.",
+    },
+    profile: {
+      title: {
+        headline:
+          "Effizientes, effektives Training ist vor allem eines: individuell zugeschnitten.",
+        summary:
+          "Ich passe bewährte Trainingsmethoden und neueste Erkenntnisse der Sportmedizin spezifisch auf dich und deine Bedürfnisse an.<br>Dadurch kannst du mess- und spürbare Fortschritte erzielen und gleichzeitig etwas für deine Gesundheit tun.<br>",
+      },
+      cv: {
+        headline: "sportlicher Werdegang",
+        certificates: {
+          fitnesstrainerA: {
+            title: "Fitnesstrainer A",
+            date: "Dezember 2017",
+          },
+          personaltrainer: {
+            title: "Personal Trainer",
+            date: "Dezember 2017",
+          },
+          medicalfitnesstrainer: {
+            title: "Medizinischer Fitnesstrainer",
+            date: "November 2017",
+          },
+          fitnesstrainerB: {
+            title: "Fitnesstrainer B",
+            date: "Oktober 2017",
+          },
+        },
+        sports: {
+          movement: {
+            title: "gezieltes Movementtraining",
+            date: "seit 2016",
+          },
+          bodyweight: {
+            title: "Body Weight Krafttraining",
+            date: "seit 2016",
+          },
+          bouldering: {
+            title: "Bouldern + Routenbau",
+            date: "seit 2012",
+          },
+          systema: {
+            title: "RMA Systema",
+            date: "3 Jahre",
+          },
+          escrima: {
+            title: "Escrima – Stockkampf",
+            date: "1 Jahr",
+          },
+          wingtsun: {
+            title: "Wing Tsun Kung Fu",
+            date: "2 Jahre",
+          },
+          karate: {
+            title: "Shotokan Karate",
+            date: "5 Jahre",
+          },
+          judo: {
+            title: "Wettkampf Judo",
+            date: "5 Jahre",
+          },
+          gymnastics: {
+            title: "Geräteturnen",
+            date: "2 Jahre",
+          },
+        },
+      },
+    },
+    pricing: {
+      title: "Preise",
+      conditions:
+        "Eine Trainingseinheit dauert 60 Minuten. Kürzere oder längere Trainingseinheiten können individuell abgesprochen werden.",
+      footer: "Das Training findet ausschließlich in meinem Studio statt:",
+      perUnit: "/ Einheit",
+      items: {
+        showDetailsButton: "Details",
+        testTraining: {
+          title: "Testtraining",
+          subTitle: "Kennenlernen und ausprobieren",
+          details:
+            '<p><small class="text-muted">Vertrauen und Sympathie sind Schlüsselelemente für ein erfolgreiches Personal Training. Ein Testtraining nach einer kurzen Absprache der Ziele bietet die Ideale Möglichkeit um sich kennen zu lernen und zu schauen ob die Chemie stimmt.</small></p>' +
+            '<p><small class="text-muted">Vielleicht weißt du aber auch noch gar nicht ob Personal Training überhaupt etwas für dich ist und willst dich durch ein allgemeines Testtraining davon überzeugen was es dir bieten kann.</small></p>',
+        },
+        singleSession: {
+          title: "Einzeleinheit",
+          subTitle: "Volle Flexibilität / persönlicher Trainingsplan",
+          details:
+            '<p><small class="text-muted">Die Einzelstunde bietet dir die Möglichkeit völlig ungebunden erstmal ein paar Trainingseinheiten durchzuführen.</small></p>' +
+            '<p><small class="text-muted">Auch die Erstellung eines persönlichen Trainingsplans für ein später selbst durchgeführtes Training oder die Überprüfung der Trainingserfolge und eine entsprechende Anpassung des aktuellen Trainingsplans können im Rahmen einer Einzelstunde erfolgen.</small></p>' +
+            '<p><small class="text-muted">Bevor wir das erste Mal zusammen trainieren (egal ob Einzelstunde oder Paket), erfolgt ein persönliches Anamnesegespräch. Dabei ermitteln wir deine Trainingsziele und Umstände um dein Training perfekt auf dich zuschneiden zu können.</small>' +
+            "<br><small>Dieses Gespräch ist in der Einzeleinheit und den Trainingspaketen enthalten.</small></p>",
+        },
+        upgrade: {
+          title: "Zusatzzeit",
+          subTitle: "je 30 Minuten Verlängerung für eine gebuchte Einheit",
+          details:
+            '<p><small class="text-muted">Richtig intensives Krafttraining? Noch einen Mobilitypart dazu? Wenn du mehr Themenfelder abdecken oder intensiver trainieren möchtest, kannst du eine bereits gebuchte Einheite für einen günstigen Stundenpreis verlängern.</small></p>' +
+            '<p><small class="text-muted">Die Verlängerung muss bei der Terminabsprache angegeben werden, damit ich die Einheit richtig einplanen kann und kann auch dauerhaft für eines der Pakete gebucht werden.</small></p>',
+        },
+        consultation: {
+          title: "Trainingsplan (online)",
+          subTitle: "Body Weight Krafttraining",
+          details:
+            '<p><small class="text-muted">Du hast kein Problem damit dich zum Training zu motivieren und hast auch schon eine Idee was du erreichen möchtest aber dir fehlt die richtige Idee wie du effektiv Kraft und Muskelaufbau trainieren kannst? Dann brauchst du vielleicht nur den richtigen Trainingsplan.</small></p>' +
+            '<p><small class="text-muted">Nach einem kurzen online Ausstausch per Mail baue ich dir einen Trainingsplan zusammen, der mit einfachen Mitteln auch zu Hause umgesetzt werden kann. Von Vorteil für deine Performance (aber nicht zwingend notwendig) sind eine Klimmzugstange oder Ringe.</small></p>',
+        },
+        fivePack: {
+          title: "5er Paket",
+          subTitle: "inkl. Anamnesegespräch",
+        },
+        tenPack: {
+          title: "10er Paket",
+          subTitle: "inkl. Anamnesegespräch",
+        },
+        twentyPack: {
+          title: "20er Paket",
+          subTitle: "inkl. Anamnesegespräch",
+        },
+      },
+    },
+    contact: {
+      title: "Kontakt",
+      subTitle:
+        "Überzeugt, dass ich dir bei deinem Training helfen kann?<br>" +
+        "Dann schreib mir doch einfach eine Mail oder ruf mich kurz an und wir vereinbaren einen Termin.",
+      instagram: {
+        followMe: "Folge mir auf Instagram",
+      },
+    },
+  },
 }
--- i18n/en.js
+++ i18n/en.js
@@ -1,143 +1,157 @@
 module.exports = {
-	entries: {
-		language: {
-			current: {
-				code: 'en',
-				name: 'english'
-			},
-			alternate : {
-				code: 'de',
-				name: 'Deutsch',
-				link: '/'
-			}
-		},
-		navItem: {
-			trainerProfile : 'trainer',
-			pricing: 'pricing',
-			contact: 'contact'
-		},
-		philosophy: {
-			title: 'Your customised, healthy way to enhanced quality of life, functional strength and improved motor control.'
-		},
-		profile: {
-			title: {
-				headline: 'Efficient, effective training has to be one thing above all else: customized.',
-				summary: 'I tailor well proven methods of training and latest knowledge of sports medicine specifically to your needs.<br>Thus you will make measurable and noticeable progress while strengthening your health along the way.'
-			},
-			cv: {
-				headline: 'sports cv',
-				certificates: {
-					fitnesstrainerA: {
-						title: 'Fitness Trainer A',
-						date: 'December 2017'
-					},
-					personaltrainer: {
-						title: 'Personal Trainer',
-						date: 'December 2017'
-					},
-					medicalfitnesstrainer: {
-						title: 'Medical Fitness Trainer',
-						date: 'November 2017'
-					},
-					fitnesstrainerB: {
-						title: 'Fitness Trainer B',
-						date: 'Octobre 2017'
-					}
-				},
-				sports: {
-					movement: {
-						title: 'Movement Training',
-						date: 'since 2016'
-					},
-					bodyweight: {
-						title: 'Body Weight Strength Training',
-						date: 'since 2016'
-					},
-					bouldering: {
-						title: 'Bouldering + Route Setting',
-						date: 'since 2012'
-					},
-					systema: {
-						title: 'RMA Systema',
-						date: '3 years'
-					},
-					escrima: {
-						title: 'Eskrima – stick fighting',
-						date: '1 year'
-					},
-					wingtsun: {
-						title: 'Wing Tsun Kung Fu',
-						date: '2 years'
-					},
-					karate: {
-						title: 'Shotokan Karate',
-						date: '5 years'
-					},
-					judo: {
-						title: 'Competition Judo',
-						date: '5 years'
-					},
-					gymnastics: {
-						title: 'Gymnastics',
-						date: '2 years'
-					}
-				}
-			}
-		},
-		pricing: {
-			title: 'Pricing',
-			conditions: 'A training session lasts 60 minutes. Shorter or longer sessions can be arranged individually.',
-			footer: 'All prices apply to sessions inside of Berlin including commute. Longer trips need special agreements.',
-			perUnit: 'per session',
-			items: {
-				showDetailsButton: 'details',
-				testTraining: {
-					title: 'Test Training',
-					subTitle: 'Try it out',
-					details: '<p><small class="text-muted">Trust and sympathy are key factors for successful personal training. A test session (after a brief dialogue about your goals) is the perfect opportunity to get to know each other and see if we\'re a fit.</small></p>' +
-					'<p><small class="text-muted">Or maybe you don\'t even know if you want to engage in personal training and want to see what the benefits are with a general test training session.</small></p>'
-				},
-				singleSession: {
-					title: 'Single Session',
-					subTitle: 'full felxibility / personalized workout',
-					details: '<p><small class="text-muted">The single session provides the possibility to do a few sessions without a big commitment.</small></p>' +
-					'<p><small class="text-muted">Other options would be to create a personal workout plan to follow along on your own or reviewing the training progress and adapting an existing plan.</small></p>' +
-					'<p><small class="text-muted">Before jumping into the first workout (no matter if single session or package deal), we\'ll talk about your goals and life situation so I can ensure to tailor the training exactly to your needs.</small>' +
-						'<br><small>The requirement dialogue is included in the single session and the package deals.</small></p>'
-				},
-				upgrade: {
-					title: 'Extra Time',
-					subTitle: 'extend your booked session by 30 minutes',
-					details: '<p><small class="text-muted">You need a more intense strength training? Adding a longer mobility part sounds good? If you want to address more topics or train more intensely in a specific direction, you can extend an already booked session.</small></p>' +
-					'<p><small class="text-muted">Please make clear you need extra time when setting the appointment, so I can modify the session accordingly. It can also be booked reoccuring for a package.</small></p>'
-				},
-				consultation: {
-					title: 'Online Workout Plan',
-					subTitle: '…for body weight strength training',
-					details: '<p><small class="text-muted">You don\'t mind training on your own and know exactly what you want to achieve, but you lack the idea of how to traing strength and muscle building effectively? Then all you need is a good workout plan.</small></p>' +
-					'<p><small class="text-muted">After syncing with each other online, I\'ll set up a workout plan, that can be done with simple equipment at home. A pull up bar and rings are benefitial for your performance but not necessarily required.</small></p>'
-				},
-				fivePack: {
-					title: '5 sessions',
-					subTitle: 'including preparation dialogue'
-				},
-				tenPack: {
-					title: '10 sessions',
-					subTitle: 'including preparation dialogue'
-				},
-				twentyPack: {
-					title: '20 sessions',
-					subTitle: 'including preparation dialogue'
-				}
-			}
-		},
-		contact: {
-			title: 'Contact',
-			subTitle: 'Convinced I can help you with your training?<br>' +
-			'Just call me or send an e-mail and we\'ll make an appointment.',
-			instagram: {
-				followMe: 'Follow me on Instagram'
-			}
-		}
-	}
+  entries: {
+    language: {
+      current: {
+        code: "en",
+        name: "english",
+      },
+      alternate: {
+        code: "de",
+        name: "Deutsch",
+        link: "/",
+      },
+    },
+    navItem: {
+      trainerProfile: "trainer",
+      pricing: "pricing",
+      contact: "contact",
+    },
+    philosophy: {
+      title:
+        "Your customised, healthy way to enhanced quality of life, functional strength and improved motor control.",
+    },
+    studio: {
+      heading: "Welcome at my new training studio!",
+      description:
+        "To provide you with an even better training experience, I opened my own training studio. Aside from everythin needed for a healthy strength training, it sports a bouldering wall come December 2020. Thus movement training is now featured in full 3D.",
+    },
+    profile: {
+      title: {
+        headline:
+          "Efficient, effective training has to be one thing above all else: customized.",
+        summary:
+          "I tailor well proven methods of training and latest knowledge of sports medicine specifically to your needs.<br>Thus you will make measurable and noticeable progress while strengthening your health along the way.",
+      },
+      cv: {
+        headline: "sports cv",
+        certificates: {
+          fitnesstrainerA: {
+            title: "Fitness Trainer A",
+            date: "December 2017",
+          },
+          personaltrainer: {
+            title: "Personal Trainer",
+            date: "December 2017",
+          },
+          medicalfitnesstrainer: {
+            title: "Medical Fitness Trainer",
+            date: "November 2017",
+          },
+          fitnesstrainerB: {
+            title: "Fitness Trainer B",
+            date: "Octobre 2017",
+          },
+        },
+        sports: {
+          movement: {
+            title: "Movement Training",
+            date: "since 2016",
+          },
+          bodyweight: {
+            title: "Body Weight Strength Training",
+            date: "since 2016",
+          },
+          bouldering: {
+            title: "Bouldering + Route Setting",
+            date: "since 2012",
+          },
+          systema: {
+            title: "RMA Systema",
+            date: "3 years",
+          },
+          escrima: {
+            title: "Eskrima – stick fighting",
+            date: "1 year",
+          },
+          wingtsun: {
+            title: "Wing Tsun Kung Fu",
+            date: "2 years",
+          },
+          karate: {
+            title: "Shotokan Karate",
+            date: "5 years",
+          },
+          judo: {
+            title: "Competition Judo",
+            date: "5 years",
+          },
+          gymnastics: {
+            title: "Gymnastics",
+            date: "2 years",
+          },
+        },
+      },
+    },
+    pricing: {
+      title: "Pricing",
+      conditions:
+        "A training session lasts 60 minutes. Shorter or longer sessions can be arranged individually.",
+      footer: "Training exclusively takes place in my training studio:",
+      perUnit: "per session",
+      items: {
+        showDetailsButton: "details",
+        testTraining: {
+          title: "Test Training",
+          subTitle: "Try it out",
+          details:
+            '<p><small class="text-muted">Trust and sympathy are key factors for successful personal training. A test session (after a brief dialogue about your goals) is the perfect opportunity to get to know each other and see if we\'re a fit.</small></p>' +
+            '<p><small class="text-muted">Or maybe you don\'t even know if you want to engage in personal training and want to see what the benefits are with a general test training session.</small></p>',
+        },
+        singleSession: {
+          title: "Single Session",
+          subTitle: "full felxibility / personalized workout",
+          details:
+            '<p><small class="text-muted">The single session provides the possibility to do a few sessions without a big commitment.</small></p>' +
+            '<p><small class="text-muted">Other options would be to create a personal workout plan to follow along on your own or reviewing the training progress and adapting an existing plan.</small></p>' +
+            '<p><small class="text-muted">Before jumping into the first workout (no matter if single session or package deal), we\'ll talk about your goals and life situation so I can ensure to tailor the training exactly to your needs.</small>' +
+            "<br><small>The requirement dialogue is included in the single session and the package deals.</small></p>",
+        },
+        upgrade: {
+          title: "Extra Time",
+          subTitle: "extend your booked session by 30 minutes",
+          details:
+            '<p><small class="text-muted">You need a more intense strength training? Adding a longer mobility part sounds good? If you want to address more topics or train more intensely in a specific direction, you can extend an already booked session.</small></p>' +
+            '<p><small class="text-muted">Please make clear you need extra time when setting the appointment, so I can modify the session accordingly. It can also be booked reoccuring for a package.</small></p>',
+        },
+        consultation: {
+          title: "Online Workout Plan",
+          subTitle: "…for body weight strength training",
+          details:
+            '<p><small class="text-muted">You don\'t mind training on your own and know exactly what you want to achieve, but you lack the idea of how to traing strength and muscle building effectively? Then all you need is a good workout plan.</small></p>' +
+            '<p><small class="text-muted">After syncing with each other online, I\'ll set up a workout plan, that can be done with simple equipment at home. A pull up bar and rings are benefitial for your performance but not necessarily required.</small></p>',
+        },
+        fivePack: {
+          title: "5 sessions",
+          subTitle: "including preparation dialogue",
+        },
+        tenPack: {
+          title: "10 sessions",
+          subTitle: "including preparation dialogue",
+        },
+        twentyPack: {
+          title: "20 sessions",
+          subTitle: "including preparation dialogue",
+        },
+      },
+    },
+    contact: {
+      title: "Contact",
+      subTitle:
+        "Convinced I can help you with your training?<br>" +
+        "Just call me or send an e-mail and we'll make an appointment.",
+      instagram: {
+        followMe: "Follow me on Instagram",
+      },
+    },
+  },
 }
